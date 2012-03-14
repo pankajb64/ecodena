6843852f3038508b09ea2fb77357e93751544fb9
Pankaj Bhambhani <pankajb64@gmail.com>
2012-03-14 10:13:16 -0700

    Added two new models - Language and Compiler Version. Made appropriate changes in Compiler

--- Ecodena/Compiler/models.py
+++ Ecodena/Compiler/models.py
@@ -2,13 +2,48 @@ from django.db import models
 
 # Create your models here.
 
+class Language(models.Model, object)
+
+	languageID_f = models.AutoField(primary_key=True)
+	languageName_f = models.CharField("Language Name", max_length=40, null=False)
+
+	def getLanguageName(self):
+		return self.languageName_f
+
+	def setLanguageName(self, languageName)
+		self.languageName_f = languageName
+
+	languageName = property(getLanguageName, setLanguageName)
+
+class CompilerVersion(models.Model, object)
+	versionID_f = models.AutoField(primary_key=True)
+	versionName_f = models.CharField("Version Name (Code or Number)", max_length=40, null=False)
+	language_f = models.ForeignKey(Language, verbose_name="Language of Compilation", null=False)
+
+	def getVersionName(self):
+		return self.versionName_f
+
+	def setVersionName(self, versionName)
+		self.versionName_f = versionName
+
+	versionName = property(getVersionName, setVersionName)
+
+	def getLanguage(self):
+		return self.language_f
+
+	def setLanguage(self, language)
+		self.language_f = language
+
+	language = property(getLanguage, setLanguage)
+	
 class Compiler():
 	
-	Language = ""
-	CompilerVersion = ""
+	language = ""
+	compilerVersion = ""
 	question = None
 	attempt = None
-	
+	errorReport = None
+
 	#def getLanguage(self)
 	#def setLanguage(self, text)
 	#def getCompilerVersion(self)
