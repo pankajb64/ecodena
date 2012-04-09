6a23b80343b0cae6f51896f2713497bc57356138
Arunangshu Bhakta <arunb24@gmail.com>
2012-04-10 01:39:07 +0530

    Improved Recommendation.

--- src/Ecodena/Recommendation/admin.py
+++ src/Ecodena/Recommendation/admin.py
@@ -1,1 +1,13 @@
+from Recommendation.models import Recommendation
+from Recommendation.models import Recommended
+from django.contrib import admin
 
+
+	
+class RecommendedInline(admin.TabularInline):
+	model = Recommendation.questionList_f.through
+	
+class RecommendationAdmin(admin.ModelAdmin):
+	inlines = [RecommendedInline]
+
+admin.site.register(Recommendation,RecommendationAdmin)
